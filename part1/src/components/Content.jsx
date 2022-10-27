b3ccf93b578049977dde0a1ac389ef25fe7ac278
influ <26268158+ahmed316dev@users.noreply.github.com>
2022-10-27 14:20:26 +0300

    course info completed

---
+++ part1/src/components/Content.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Part from './Part'
+const Content = ({ parts, exercises }) => {
+  const { part1, part2, part3 } = parts
+  const { exercises1, exercises2, exercises3 } = exercises
+  return (
+    <>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </>
+  )
+}
+
+export default Content
